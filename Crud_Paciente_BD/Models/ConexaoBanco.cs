8f88d5f82c52a90f45ea6caf429c19fad1e2e157
Raphael Ferreira de Moraes <raphaelfm.2012@gmail.com>
2022-10-10 18:31:14 -0300

    Cria conexao com o banco de dados

---
+++ Crud_Paciente_BD/Models/ConexaoBanco.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient; //incluir esta linha
+
+namespace Crud_Paciente_BD.Models
+{
+    internal class ConexaoBanco
+    {
+        private string host = "localhost";
+        private string database = "basedados_pacientes";
+        private string user = "root";
+        private string password = "***";
+        private int port = 3307;
+        private MySqlConnection con;
+        private MySqlCommand cmd;
+        public ConexaoBanco()
+        {
+            conectar();
+        }
+        public void conectar()
+        {
+            string strCon = @"server=" + this.host + "; database=" + this.database + "; user=" + this.user + "; password=" + this.password + "; port=" + this.port + ";";
+            this.con = new MySqlConnection(strCon);
+            this.cmd = this.con.CreateCommand();
+            this.con.Open();
+        }
+        public void close()
+        {
+            this.con.Close();
+        }
+        public void nonQuery(string sql)
+        {
+            cmd.CommandText = sql;
+            cmd.ExecuteNonQuery();
+        }
+        public MySqlDataReader Query(string sql)
+        {
+            this.cmd.CommandText = sql;
+            return this.cmd.ExecuteReader();
+        }
+    }
+}
